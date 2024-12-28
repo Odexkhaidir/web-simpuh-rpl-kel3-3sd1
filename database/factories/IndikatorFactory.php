c9aad02c14ac2f8aebf500e7a909190a78937463
Makaareeem <munamademak123@gmail.com>
2024-12-28 14:51:20 +0700

    memperbaiki indikatorfactory

--- database/factories/IndikatorFactory.php
+++ database/factories/IndikatorFactory.php
@@ -25,11 +25,11 @@ class IndikatorFactory extends Factory
             'kode_indikator' => $this->faker->word(),
             'nama_indikator' => $this->faker->word(),
             'deskripsi' => $this->faker->text(),
-            'persentase' => $this->faker->numberBetween(-10000, 10000),
+            'persentase' => $this->faker->numberBetween(-10000, 10000), # IF(SUM(NilaiAllSub)>0,(SUMIF(NilaiAllSub,">0",PersenAllSub)/3),0)
             'tahun' => $this->faker->numberBetween(-10000, 10000),
             'kode_desa' => $this->faker->numberBetween(-10000, 10000),
-            'timestamp' => $this->faker->dateTime(),
             'desa_id' => Desa::factory(),
+            'timestamp' => $this->faker->dateTime(),
         ];
     }
 }
